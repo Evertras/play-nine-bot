3b04f9e2c855a7f402e9fc407da45463fcb5de6b
Brandon Fulljames <bfullj@gmail.com>
2025-08-05 17:02:19 -0700

    Put player boards side by side with lipgloss styles

--- cmd/playnine/view.go
+++ cmd/playnine/view.go
@@ -4,51 +4,72 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/evertras/play-nine-bot/playnine"
 )
 
-func stringifyBoardState(state playnine.PlayerBoard) string {
-	var b strings.Builder
+func renderPlayerBoards(game playnine.Game) string {
 
-	for i, c := range state {
-		if c.FaceUp {
-			fmt.Fprintf(&b, "%2d ", c.Card)
-		} else {
-			b.WriteString(" - ")
+	stringifyBoardState := func(state playnine.PlayerBoard) string {
+		var b strings.Builder
+
+		for i, c := range state {
+			if c.FaceUp {
+				fmt.Fprintf(&b, "%2d ", c.Card)
+			} else {
+				b.WriteString(" - ")
+			}
+
+			if i == playnine.PlayerBoardSize/2-1 {
+				b.WriteRune('\n')
+			}
 		}
 
-		if i == playnine.PlayerBoardSize/2-1 {
-			b.WriteRune('\n')
+		return b.String()
+	}
+
+	states := game.PlayerStates()
+
+	boards := make([]string, len(states))
+
+	for i, state := range states {
+		var b strings.Builder
+
+		style := lipgloss.NewStyle().PaddingLeft(1)
+
+		if i != len(states)-1 {
+			style = style.PaddingRight(1)
 		}
-	}
 
-	return b.String()
-}
-
-func (m model) View() string {
-	var b strings.Builder
-
-	newline := func() {
-		b.WriteRune('\n')
-	}
-
-	fmt.Fprintf(&b, "Discard: %2d", m.game.AvailableDiscard())
-
-	for i, state := range m.game.PlayerStates() {
-		newline()
-		newline()
 		curBoard := state.CurrentBoard()
-		if m.game.CurrentPlayerIndex() == i {
+
+		if game.CurrentPlayerIndex() == i {
 			b.WriteString("> ")
 		} else {
 			b.WriteString("  ")
 		}
 		fmt.Fprintf(&b, "Player #%d", i+1)
-		newline()
+		b.WriteRune('\n')
 		b.WriteString(stringifyBoardState(curBoard))
-		newline()
+		b.WriteRune('\n')
 		fmt.Fprintf(&b, "Score: %d", curBoard.ScoreVisible())
+
+		boards[i] = style.Render(b.String())
 	}
+
+	return lipgloss.JoinHorizontal(lipgloss.Bottom, boards...)
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	playerBoards := renderPlayerBoards(m.game)
+
+	border := lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder())
+
+	b.WriteString(border.Render(playerBoards))
+
+	fmt.Fprintf(&b, "\n\nDiscard: %2d", m.game.AvailableDiscard())
 
 	return b.String()
 }
